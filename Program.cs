0fb4b07da9bbb3af5e57dc1e8925724e72f20424
Kalid-N <107488332+Mckali-2k@users.noreply.github.com>
2022-07-08 09:28:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.LENGTH = 5;
+            r.WIDTH = 6;
+            Console.WriteLine($"The Area of The Rectagle is {r.calculateArea()}");
+
+            Circle c = new Circle();
+            c.RADIUS = 7;
+
+            Console.WriteLine($"\nThe Area of The Circle is {c.calculateArea()}");
+            Console.WriteLine($"\nThe Perimeter of The Circle is {c.calculatePerimeter()}");
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract double calculateArea();
+
+        public abstract double calculatePerimeter();
+    }
+
+    class Rectangle : Shape
+    {
+        private double length, width;
+        // property
+        public double LENGTH
+        {
+            set { length = value; }
+            get { return length; }
+        }
+
+        public double WIDTH
+        {
+            set { width = value; }
+            get { return width; }
+        }
+
+
+        public override double calculateArea()
+        {
+            return length * width;
+        }
+
+        public override double calculatePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Circle : Shape
+    {
+        static double pi = 3.14159265359;
+
+        private double radius;
+          public double RADIUS
+          {
+            set { radius = value; }
+            get { return radius; }
+          }
+
+
+        public override double calculateArea()
+        {
+            return (pi * (radius * radius));
+        }
+
+        public override double calculatePerimeter()
+        {
+            return (2 * (pi) * (radius));
+        }
+    }
+}
